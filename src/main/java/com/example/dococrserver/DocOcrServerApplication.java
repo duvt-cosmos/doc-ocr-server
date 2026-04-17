18654c6bc9761f322691079b059613b646148553
tinduvo <votindu26062003@gmail.com>
2026-04-17 10:55:50 +0700

    chore: enable jpa auditing

--- src/main/java/com/example/dococrserver/DocOcrServerApplication.java
+++ src/main/java/com/example/dococrserver/DocOcrServerApplication.java
@@ -2,9 +2,11 @@ package com.example.dococrserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DocOcrServerApplication {
 
     public static void main(String[] args) {
